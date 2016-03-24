2d880185305d1e15a3aff16a37f521d4ebc87141
Jakub Klama <jakub@ixsystems.com>
2016-03-24 22:23:36 +0100

    Fix overly strict checking of smbd_check_access_rights() return value.
    Checking for !NT_STATUS_IS_OK(status) will prevent accessing snapshot
    dir if smbd_check_access_rights() returns eg. NT_STATUS_NOT_SUPPORTED.
    
    Related samba ticket: https://bugzilla.samba.org/show_bug.cgi?id=11658
    
    Ticket: #14257
    (cherry picked from commit ec9486f19013d48fbceea957e16cbe632872bbda)

--- source3/modules/vfs_shadow_copy2.c
+++ source3/modules/vfs_shadow_copy2.c
@@ -1841,7 +1841,7 @@ static bool check_access_snapdir(struct vfs_handle_struct *handle,
 					&smb_fname,
 					false,
 					SEC_DIR_LIST);
-	if (!NT_STATUS_IS_OK(status)) {
+	if (NT_STATUS_EQUAL(status, NT_STATUS_ACCESS_DENIED)) {
 		DEBUG(0,("user does not have list permission "
 			"on snapdir %s\n",
 			smb_fname.base_name));
