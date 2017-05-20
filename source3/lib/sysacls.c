1c7542ff4904b729e311e17464ee76582760c219
John Hixson <john@ixsystems.com>
2017-05-19 17:04:26 -0700

    Kick samba a little bit into understanding NFSv4 ACL's
    Ticket: #23566

--- source3/lib/sysacls.c
+++ source3/lib/sysacls.c
@@ -38,6 +38,16 @@
 #include "modules/vfs_hpuxacl.h"
 #endif
 
+/*
+ * NFSv4 ACL's should be understood and a first class citizen. Work
+ * needs to be done in librpc/idl/smb_acl.idl for this to occur.
+ */
+#if defined(HAVE_LIBSUNACL) && defined(FREEBSD)
+#if 0
+#include "modules/nfs4_acls.h"
+#endif
+#endif
+
 #undef  DBGC_CLASS
 #define DBGC_CLASS DBGC_ACLS
 
