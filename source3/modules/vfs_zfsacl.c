7c94f58c252292a2d0b071337622d5aba87a0cd1
Andrew <walker.aj325@gmail.com>
2017-12-12 15:05:27 -0600

    make Timur happy - use bool
    (cherry picked from commit acdfe7cf05d4d17207287d4c39ef13e1d99c1521)

--- source3/modules/vfs_zfsacl.c
+++ source3/modules/vfs_zfsacl.c
@@ -94,7 +94,7 @@ static NTSTATUS zfs_get_nt_acl_common(struct connection_struct *conn,
 	const SMB_STRUCT_STAT *psbuf = NULL;
 	int ret;
 	bool is_dir;
-	uint16_t inherited_present;
+	bool inherited_present;
 
 	if (VALID_STAT(smb_fname->st)) {
 		psbuf = &smb_fname->st;
@@ -155,7 +155,7 @@ static NTSTATUS zfs_get_nt_acl_common(struct connection_struct *conn,
 		 * to set DACL_PROTECTED in the security descriptor.
 		 */
 		if(aceprop.aceFlags & ACE_INHERITED_ACE) {
-			inherited_present = 1;
+			inherited_present = true;
 		}
 
 		/*
