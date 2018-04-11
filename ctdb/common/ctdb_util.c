aba74d4e69d3be4227a50e2d84977f43e7ba758c
John Hixson <john@ixsystems.com>
2018-04-11 14:34:37 -0400

    sin_len -> sin6_len

--- ctdb/common/ctdb_util.c
+++ ctdb/common/ctdb_util.c
@@ -390,7 +390,7 @@ void ctdb_canonicalize_ip(const ctdb_sock_addr *ip, ctdb_sock_addr *cip)
 		} else {
 			cip->ip6.sin6_family = AF_INET6;
 #ifdef HAVE_SOCK_SIN_LEN
-			cip->ip6.sin_len = sizeof(ctdb_sock_addr);
+			cip->ip6.sin6_len = sizeof(ctdb_sock_addr);
 #endif
 			cip->ip6.sin6_port   = ip->ip6.sin6_port;
 			memcpy(&cip->ip6.sin6_addr,
