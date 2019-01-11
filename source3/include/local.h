bbdb56c4987d831fca91f6cd34fb74784a5ea882
Andrew <awalker@ixsystems.com>
2019-01-11 15:16:10 -0500

    Make samba default listen backlog value -1 on FreeBSD

--- source3/include/local.h
+++ source3/include/local.h
@@ -176,7 +176,18 @@
 #define BACKTRACE_STACK_SIZE 64
 
 /* size of listen() backlog in smbd */
+#if defined (FREEBSD)
+#define SMBD_LISTEN_BACKLOG -1
+#else 
 #define SMBD_LISTEN_BACKLOG 50
+#endif
+
+/* size of listen() default backlog */
+#if defined (FREEBSD)
+#define DEFAULT_LISTEN_BACKLOG -1
+#else
+#define DEFAULT_LISTEN_BACKLOG 5
+#endif
 
 /* Number of microseconds to wait before a sharing violation. */
 #define SHARING_VIOLATION_USEC_WAIT 950000
